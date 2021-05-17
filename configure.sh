d8f38ace7366f1148f2e9234922291de0b006e83
Tim Gebauer <tim.gebauer@tu-dortmund.de>
2021-05-17 13:37:22 +0200

    optimizations

--- configure.sh
+++ configure.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #cd ../../ && ./waf clean && ./waf configure --disable-python --enable-examples --enable-module=lte
-#cd ../../ && ./waf configure --disable-python --enable-examples --enable-module=lte
+#cd ../../ && ./waf clean && CXXFLAGS="-g -march=native" ./waf configure --disable-python --enable-examples --enable-module=lte
 
 
 #cd ../../ && ./waf distclean && CXXFLAGS="-O3 -w" ./waf -d optimized configure --enable-static --enable-examples --enable-modules=lte
 #cd ../../ && ./waf clean && ./waf distclean && 
 #cd ../../ && ./waf clean && CXXFLAGS="-O3 -g" ./waf -d optimized configure --enable-examples --enable-modules=lte --disable-python
 
-cd ../../ && ./waf clean && CXXFLAGS="-O3" ./waf -d optimized configure --enable-examples --enable-modules=lte --disable-python
+#cd ../../ && ./waf clean && CXXFLAGS="-Ofast" ./waf -d optimized configure --enable-examples --enable-modules=lte --disable-python --enable-static
+cd ../../ && ./waf clean && CXXFLAGS="-g -O3 -march=native" ./waf -d optimized configure --enable-examples --enable-modules=lte --disable-python --enable-static
+#cd ../../ && ./waf clean && CXX="clang++" CXXFLAGS="-Ofast" ./waf -d optimized configure --enable-examples --enable-modules=lte --disable-python --enable-static
